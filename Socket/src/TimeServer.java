0d55502db66c5fa3d5b606acda06c1165cc13976
LAPTOP-I7228BSL\claud <claudiaesther.rubio@gmail.com>
2023-12-13 18:41:51 +0100

    crear socket de servidor

---
+++ Socket/src/TimeServer.java
@@ -0,0 +1,30 @@
+import java.util.Date;
+import java.net.*;
+import java.io.*;
+
+// Socket server
+public class TimeServer {
+
+   public static void main(String[] args) {
+       if (args.length < 1) return;
+
+       int port = Integer.parseInt(args[0]);
+
+       try (ServerSocket serverSocket = new ServerSocket(port)) { // intento crear el serversocket
+
+           System.out.println("Server is listening on port " + port);
+
+           while (true) {
+               Socket socket = serverSocket.accept(); //creacion del socket, el programa se queda esperando aqui hasta recibir conexiones
+               System.out.println("New client connected");
+               OutputStream output = socket.getOutputStream();
+               PrintWriter writer = new PrintWriter(output, true);
+               writer.println(new Date().toString());
+           }
+
+       } catch (IOException ex) {
+           System.out.println("Server exception: " + ex.getMessage());
+           ex.printStackTrace();
+       }
+   }
+}
